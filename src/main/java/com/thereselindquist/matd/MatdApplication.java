b810bc6fd518f94e49251affe08d38224e09897d
thereselindquist <lindquist20@gmail.com>
2023-02-12 12:45:42 +0100

    first commit

---
+++ src/main/java/com/thereselindquist/matd/MatdApplication.java
@@ -0,0 +1,13 @@
+package com.thereselindquist.matd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatdApplication.class, args);
+	}
+
+}
